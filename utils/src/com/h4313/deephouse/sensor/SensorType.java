82005b7befdc23a96abb92d0877764badde96b10
Julien <Julien@134.214.222.55>
2014-01-24 10:03:57 +0100

    Ajout de typeFrame

--- utils/src/com/h4313/deephouse/sensor/SensorType.java
+++ utils/src/com/h4313/deephouse/sensor/SensorType.java
@@ -2,20 +2,34 @@ package com.h4313.deephouse.sensor;
 
 public enum SensorType {
 
-	TEMPERATURE(true), SWITCH(true), LIGHT(true), PRESENCE(true), HUMIDITY(
-			false), NOISE(false), SMOKE(false), SMELL(false);
+	TEMPERATURE(true,""), 
+	SWITCH(true,"A55A0B05"),
+	LIGHT(true,""), 
+	PRESENCE(true,""), 
+	HUMIDITY(false,""), 
+	NOISE(false,""), 
+	SMOKE(false,""), 
+	SMELL(false,"");
 
 	/*
 	 * true if the sensor is a physical one false if it's a virtual one
 	 */
 	private final boolean physical;
+	
+	//typeFrame has to be 4 byte long => 8 hexa characters
+	private final String typeFrame;
 
-	SensorType(boolean physical) {
+	SensorType(boolean physical, String typeFrame) {
+		this.typeFrame = typeFrame;
 		this.physical = physical;
 	}
 
 	public boolean isPhysical() {
 		return physical;
+	}
+	
+	public String getTypeFrame() {
+		return typeFrame;
 	}
 
 }
