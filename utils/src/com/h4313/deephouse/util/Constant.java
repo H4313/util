4091f6e883fb9c2133680bdf5e08350650c216fe
macbric <macbric>
2014-01-29 19:53:44 +0100

    Définition d'un calendrier où 1 minute réelle vaut 1 journée pour l'environnement de simulation

--- utils/src/com/h4313/deephouse/util/Constant.java
+++ utils/src/com/h4313/deephouse/util/Constant.java
@@ -21,4 +21,15 @@ public abstract class Constant
 	 * TCP
 	 */
 	public static final int TCP_FRAME_LENGTH = 28;
+	
+    /**
+     * DeepHouseCalendar
+     */
+    public static final int DEFAULT_YEAR = 2014;
+    public static final int DEFAULT_MONTH = 1;
+    public static final int DEFAULT_DAY = 1;
+    public static final int DEFAULT_HOUR = 0;
+    public static final int DEFAULT_MINUTE = 0;
+    public static final int DEFAULT_SECOND = 0;
+    public static final int TIME_FACTOR = 1440; // current time = real time * X
 }
