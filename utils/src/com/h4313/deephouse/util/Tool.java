2d67ddad73ac45604610f8323c66f2936aac384f
macbric <macbric>
2014-02-17 12:42:23 +0100

    Ajout d'une fonction de random d'entiers

--- utils/src/com/h4313/deephouse/util/Tool.java
+++ utils/src/com/h4313/deephouse/util/Tool.java
@@ -1,5 +1,6 @@
 package com.h4313.deephouse.util;
 
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,6 +25,28 @@ public abstract class Tool {
 		Pattern pattern = Pattern.compile(PATTERN);
 		Matcher matcher = pattern.matcher(port);
 		return matcher.matches();
+	}
+	
+	/**
+	 * Returns a pseudo-random number between min and max, inclusive.
+	 * The difference between min and max can be at most
+	 * <code>Integer.MAX_VALUE - 1</code>.
+	 *
+	 * @param min Minimum value
+	 * @param max Maximum value.  Must be greater than min.
+	 * @return Integer between min and max, inclusive.
+	 * @see java.util.Random#nextInt(int)
+	 */
+	public static int randInt(int min, int max) {
+
+	    // Usually this can be a field rather than a method variable
+	    Random rand = new Random();
+
+	    // nextInt is normally exclusive of the top value,
+	    // so add 1 to make it inclusive
+	    int randomNum = rand.nextInt((max - min) + 1) + min;
+
+	    return randomNum;
 	}
 
 	public static boolean testTypeConnectivity(SensorType sensorType,
