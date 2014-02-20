18eedd5ae97723128caecd80701427c20bd0e40e
macbric <macbric>
2014-02-20 15:43:27 +0100

    ajout d'un id de serialization

--- utils/src/com/h4313/deephouse/exceptions/DeepHouseDuplicateException.java
+++ utils/src/com/h4313/deephouse/exceptions/DeepHouseDuplicateException.java
@@ -2,6 +2,11 @@ package com.h4313.deephouse.exceptions;
 
 public class DeepHouseDuplicateException extends DeepHouseException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1824779941773151132L;
+
 	public DeepHouseDuplicateException(String msg) {
 		super(msg);
 	}
